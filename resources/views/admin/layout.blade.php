3e89ea15ea722ba2e8e39fbf3f17d8f00ffec0e6
Muhammad Hilmi <24FTT1072@student.pb.edu.bn>
2025-11-08 18:54:28 +0800

    final after present

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -70,10 +70,6 @@
             <h2 class="text-xl font-semibold text-gray-800">@yield('header', 'Admin')</h2>
             <div class="flex items-center space-x-4">
                 <div class="relative">
-                    <i data-feather="search" class="absolute left-3 top-2.5 text-gray-400"></i>
-                    <input type="text" placeholder="Search..." class="pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#68D6EC] focus:border-transparent">
-                </div>
-                <div class="relative">
                     <i data-feather="bell" class="text-gray-500 cursor-pointer hover:text-[#68D6EC]"></i>
                     <span class="absolute top-0 right-0 h-2 w-2 rounded-full bg-red-500"></span>
                 </div>
